0a48d12c676d1c4d4cb6c7af9bd8df461e7d8d22
Korjaw Korjaw <kornel.jaworski@gmail.com>
2023-07-28 01:39:15 +0200

    Add project files.

---
+++ Week2_Lesson7_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+            Napisz program w C#, który sprawdzi czy podana przez użytkownika liczba jest dodatnia czy ujemna.
+            Dane testowe : 14 
+            Rezultat w terminalu : 14 jest liczbą dodatnią
+        */
+
+        Console.WriteLine("Podaj liczbę:");
+        
+        int chosenNumber = Int32.Parse(Console.ReadLine());
+
+        if (chosenNumber > 0)
+        {
+            Console.WriteLine($"{chosenNumber} jest liczbą dodatnią");
+        }
+        else if (chosenNumber == 0)
+        {
+            Console.WriteLine($"{chosenNumber} nie jest ani dodatnie ani ujemne");
+        }
+        else
+        {
+            Console.WriteLine($"{chosenNumber} jest liczbą ujemną");
+        }
+    }
+
+}
